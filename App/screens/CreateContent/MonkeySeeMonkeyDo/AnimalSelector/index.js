f93d6c5430335f0b9844ac1a5f38e5f255cb9087
thibaudszy <thivaudszy@gmail.com>
2021-02-03 17:22:05 +0100

    The animal selector can display 2 random animals

--- App/screens/CreateContent/MonkeySeeMonkeyDo/AnimalSelector/index.js
+++ App/screens/CreateContent/MonkeySeeMonkeyDo/AnimalSelector/index.js
@@ -1,16 +1,44 @@
 import React from "react";
-import {View, Text, TouchableWithoutFeedback, FlatList} from "react-native";
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+  FlatList,
+} from "react-native";
 import styles from "@styles/styles";
+import {chooseAnimalAtRandom} from "../../../../assets/animalList";
+import ActivityCard from "../../../../components/StoryCard";
 
 export default function AnimalSelector() {
+  const {animal: animal1, picture: animal1Picture} = chooseAnimalAtRandom();
+  const {animal: animal2, picture: animal2Picture} = chooseAnimalAtRandom();
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "space-evenly",
+        justifyContent: "center",
       }}
     >
       <Text style={[styles.title, {marginTop: 0}]}>{"Animal selector \n"}</Text>
+      {/* <TouchableWithoutFeedback> */}
+      <Image
+        style={{
+          width: 66,
+          height: 58,
+          alignSelf: "center",
+        }}
+        source={animal1Picture}
+      />
+      <Image
+        style={{
+          width: 66,
+          height: 58,
+          alignSelf: "center",
+        }}
+        source={animal2Picture}
+      />
+      {/* </TouchableWithoutFeedback> */}
     </View>
   );
 }
