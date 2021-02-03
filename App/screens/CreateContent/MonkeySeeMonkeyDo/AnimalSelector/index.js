f46b6759bda975bc8f1c2bfa31c6c9ab4c9422ec
thibaudszy <thivaudszy@gmail.com>
2021-02-03 17:57:59 +0100

    Adds ability to relaod the 2 random animals.

--- App/screens/CreateContent/MonkeySeeMonkeyDo/AnimalSelector/index.js
+++ App/screens/CreateContent/MonkeySeeMonkeyDo/AnimalSelector/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   View,
   Text,
@@ -11,13 +11,17 @@ import {chooseAnimalAtRandom} from "../../../../assets/animalList";
 import ActivityCard from "../../../../components/StoryCard";
 
 export default function AnimalSelector() {
+  const [refreshSwitch, setRefreshSwitch] = useState(true);
   const {animal: animal1, picture: animal1Picture} = chooseAnimalAtRandom();
   const {animal: animal2, picture: animal2Picture} = chooseAnimalAtRandom();
+  useEffect(() => {
+    //refreshes the component to get new animals
+  }, [refreshSwitch]);
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "space-around",
       }}
     >
       <Text style={[styles.title, {marginTop: 0}]}>{"Animal selector \n"}</Text>
@@ -30,6 +34,7 @@ export default function AnimalSelector() {
         }}
         source={animal1Picture}
       />
+
       <Image
         style={{
           width: 66,
@@ -39,6 +44,13 @@ export default function AnimalSelector() {
         source={animal2Picture}
       />
       {/* </TouchableWithoutFeedback> */}
+      <TouchableWithoutFeedback
+        onPress={() => setRefreshSwitch(!refreshSwitch)}
+      >
+        <View style={[styles.loginButton, {marginBottom: "5%"}]}>
+          <Text style={styles.loginButtonText}>I want other animals</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
