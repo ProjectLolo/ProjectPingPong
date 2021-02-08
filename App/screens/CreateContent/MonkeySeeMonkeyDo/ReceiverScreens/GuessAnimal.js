4ff962acd740c81d589aac46b2a8655f906dff5d
carlijnweijer <carlijnweijer@gmail.com>
2021-02-08 18:40:03 +0100

    Display possible answers

--- App/screens/CreateContent/MonkeySeeMonkeyDo/ReceiverScreens/GuessAnimal.js
+++ App/screens/CreateContent/MonkeySeeMonkeyDo/ReceiverScreens/GuessAnimal.js
@@ -1,7 +1,8 @@
 import colors from "@assets/colors";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { getAnimalPicture } from "../../../../assets/animalList";
 import { generateAnimalsAtRandom } from "../SenderScreens/helpers";
 
 const stylesNew = StyleSheet.create({
@@ -19,15 +20,48 @@ const stylesNew = StyleSheet.create({
   },
 });
 
+const Item = ({ animal }) => {
+  return (
+    <View
+      style={{
+        width: 150,
+        height: 150,
+        backgroundColor: colors.ltPink,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        margin: 10,
+        padding: 10,
+        borderRadius: 12,
+      }}
+    >
+      <Image
+        style={{ resizeMode: "contain", height: "90%" }}
+        source={getAnimalPicture(animal)}
+      />
+      <Text>{animal}</Text>
+    </View>
+  );
+};
+
 export default function GuessAnimal({ route, navigation }) {
   const { animal } = route.params;
   const wrongAnswers = generateAnimalsAtRandom(3, [animal]);
 
+  console.log("route", route);
   console.log("animal is", animal);
   console.log("wronganswers", wrongAnswers);
 
   const possibleAnswers = wrongAnswers.concat(animal);
   console.log("possible answers", possibleAnswers);
+  console.log("animal again", animal);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Item animal={item} />
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={stylesNew.container}>
@@ -37,11 +71,20 @@ export default function GuessAnimal({ route, navigation }) {
           <Text>{animal}</Text>
         </View>
       </TouchableOpacity>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={possibleAnswers}
+          keyExtractor={(_, i) => i}
+          renderItem={renderItem}
+          numColumns={2}
+        />
+      </View>
     </SafeAreaView>
   );
 }
 
-//random set of 3 animals
-//push right animal onto the array of 3 random animals
-//put right animal in temp state
-//match right and wrong with if or switch statement
+//flatlist with possibleAnswers
+//make flatlist items clickable
+//write function that checks if answer is correct
+//if answer correct navigate to correct screeen
+//if answer is incorrect turn red
